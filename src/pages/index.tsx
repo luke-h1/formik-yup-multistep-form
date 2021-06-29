b61faba1e9c50d887f999a1e0cae01db5774a21e
luke-h1 <luke.howsam@yahoo.com>
2021-06-29 20:08:38 +0100

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Card, CardContent, Typography } from '@material-ui/core';
+
+export default function Home() {
+  return (
+      <Card>
+        <CardContent>
+          <Typography variant='h1'>Hello</Typography>
+        </CardContent>
+      </Card>
+  )
+}
