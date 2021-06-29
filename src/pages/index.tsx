7bd0e118cdbafe260aa035069217f5f15f72a915
luke-h1 <luke.howsam@yahoo.com>
2021-06-29 20:17:51 +0100

    init form create - no validation, no multi step

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,55 @@
-import { Card, CardContent, Typography } from '@material-ui/core';
+import {
+  Card,
+  CardContent,
+} from "@material-ui/core";
+import React from "react";
+import { Formik, Form, Field } from "formik";
+import { TextField, CheckboxWithLabel} from 'formik-material-ui';
+
+interface FormValues {
+  firstName: string;
+  lastName: string;
+  millionaire: boolean;
+  money: number;
+  description: string;
+}
 
 export default function Home() {
   return (
-      <Card>
-        <CardContent>
-          <Typography variant='h1'>Hello</Typography>
-        </CardContent>
-      </Card>
-  )
+    <Card>
+      <CardContent>
+        <Formik<FormValues>
+          initialValues={{
+            firstName: "",
+            lastName: "",
+            millionaire: false,
+            money: 0,
+            description: "",
+          }}
+          onSubmit={() => {}}
+        >
+          <Form>
+            <Field name="firstName" component={TextField} label="First Name" />
+            <Field name="lastName" component={TextField} label="Last Name" />
+            <Field
+              name="millionaire"
+              component={CheckboxWithLabel}
+              Label={{ label: "Checkbox" }}
+            />
+            <Field
+              name="money"
+              component={TextField}
+              label="money"
+              type="number"
+            />
+            <Field
+              name="description"
+              component={TextField}
+              label="All the money you have"
+            />
+          </Form>
+        </Formik>
+      </CardContent>
+    </Card>
+  );
 }
