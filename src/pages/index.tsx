7631956c37da509265c435ebc4a8455aeb1dcbad
luke-h1 <luke.howsam@yahoo.com>
2021-06-29 20:31:40 +0100

    validation + start on multi step

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,8 @@
-import {
-  Card,
-  CardContent,
-} from "@material-ui/core";
+import { Card, CardContent } from "@material-ui/core";
 import React from "react";
-import { Formik, Form, Field } from "formik";
-import { TextField, CheckboxWithLabel} from 'formik-material-ui';
-
+import { Formik, Form, Field, FormikConfig, FormikValues } from "formik";
+import { TextField, CheckboxWithLabel } from "formik-material-ui";
+import { number, mixed, object } from "yup";
 interface FormValues {
   firstName: string;
   lastName: string;
@@ -19,6 +16,18 @@ export default function Home() {
     <Card>
       <CardContent>
         <Formik<FormValues>
+          validationSchema={object({
+            money: mixed().when("millionaire", {
+              is: true,
+              then: number()
+                .required()
+                .min(
+                  1_000_000,
+                  "Because you said you are a millionaire you need to have 1 million"
+                ),
+              otherwise: number().required(),
+            }),
+          })}
           initialValues={{
             firstName: "",
             lastName: "",
@@ -28,28 +37,52 @@ export default function Home() {
           }}
           onSubmit={() => {}}
         >
-          <Form>
-            <Field name="firstName" component={TextField} label="First Name" />
-            <Field name="lastName" component={TextField} label="Last Name" />
-            <Field
-              name="millionaire"
-              component={CheckboxWithLabel}
-              Label={{ label: "Checkbox" }}
-            />
-            <Field
-              name="money"
-              component={TextField}
-              label="money"
-              type="number"
-            />
-            <Field
-              name="description"
-              component={TextField}
-              label="All the money you have"
-            />
+          <Form autoCapitalize="off">
+            <div>
+              <Field
+                name="firstName"
+                component={TextField}
+                label="First Name"
+              />
+              <Field name="lastName" component={TextField} label="Last Name" />
+              <Field
+                name="millionaire"
+                component={CheckboxWithLabel}
+                Label={{ label: "I am a millionaire" }}
+                type="checkbox"
+              />
+            </div>
+            <div>
+              <Field
+                name="money"
+                component={TextField}
+                label="money"
+                type="number"
+              />
+            </div>
+            <div>
+              <Field
+                name="description"
+                component={TextField}
+                label="All the money you have"
+              />
+            </div>
           </Form>
         </Formik>
       </CardContent>
     </Card>
   );
+}
+export function FormikStepper({children, ...props}: FormikConfig<FormikValues>) {
+return (
+  <Formik
+  {...props}
+
+  onSubmit={() => {}}
+>
+  <Form autoCapitalize="off">
+   {children}
+  </Form>
+</Formik>
+)
 }
